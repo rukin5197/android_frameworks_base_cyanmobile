7433475b18cc1b1c36c406e9995ea5793e6166a3
Karl Denninger <karl@denninger.net>
2011-09-07 14:24:01 -0500

    Bugfix - PowerManagerService.java

--- services/java/com/android/server/PowerManagerService.java
+++ services/java/com/android/server/PowerManagerService.java
@@ -2742,7 +2742,7 @@ class PowerManagerService extends IPowerManager.Stub
                     break;
                 }
             }
-	    return levels[i];
+	    return values[i];
         } catch (Exception e) {
             // guard against null pointer or index out of bounds errors
             Slog.e(TAG, "Values array must be non-empty and must be one element longer than "
@@ -2936,8 +2936,6 @@ class PowerManagerService extends IPowerManager.Stub
                        mLastLcdValue,
                        (mCustomLightEnabled ? mCustomLightLevels : mAutoBrightnessLevels),
                        (mCustomLightEnabled ? mCustomLcdValues : mLcdBacklightValues));
-                }
-
                 int buttonValue = getAutoBrightnessValue(value, mLastButtonValue,
                         (mCustomLightEnabled ? mCustomLightLevels : mAutoBrightnessLevels),
                         (mCustomLightEnabled ? mCustomButtonValues : mButtonBacklightValues));
